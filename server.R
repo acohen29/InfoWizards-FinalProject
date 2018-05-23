cf2d48c7ec07c939ca2f33d6440f6038f1499c6b
Alexander Cohen <acohen29@uw.edu>
2018-05-23 16:16:48 -0700

    Add test GET call

--- server.R
+++ server.R
@@ -1,7 +1,10 @@
 library(shiny)
+library(httr)
+source("key.R")
 
 func <- function(input, output) {
-  
+  #url <- paste0("https://api.mysportsfeeds.com/v1.2/pull/nfl/", input$season, "/player_injuries.csv")
+  #output$table <- renderTable(GET(url), add_headers(Authorization= paste("Basic", msf_credentials)))
 }
 
 shinyServer(func)
--- ui.R
+++ ui.R
@@ -1,7 +1,16 @@
 library(shiny)
+library(httr)
 
 ui <- fluidPage(
-  titlePanel("Title")
+  titlePanel("Title"),
+  sidebarLayout(
+    sidebarPanel(
+      textInput('season', "Enter Season (Ex: '2016-2017')")
+    ),
+    mainPanel(
+      tableOutput('table')
+    )
+  )
 )
 
 shinyUI(ui)
