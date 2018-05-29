1e35691130d273114a7d42af5d5575fac641687e
Alexander Cohen <acohen29@uw.edu>
2018-05-29 15:19:19 -0700

    Further additions

--- server.R
+++ server.R
@@ -6,22 +6,70 @@ source("key.R")
 
 func <- function(input, output) {
   reactive <- reactiveValues()
-  reactive$result <- data.frame("")
+  reactive$injury_table <- data.frame("")
+  reactive$team_table <- data.frame("")
+  reactive$positions <- c("")
+  
+  #=====================================================#
+  # GETS the API Databases when the desired year changes#
+  #=====================================================#
+  
   observeEvent(input$season, {
-    url <- paste0("https://api.mysportsfeeds.com/v1.2/pull/nfl/", input$season, "-regular/player_injuries.json")
-    response <- GET(url, add_headers('Content-Type' = "application/json"),
-                    authenticate(msf_user, msf_pass))
+    base_url <- paste0("https://api.mysportsfeeds.com/v1.2/pull/nfl/", input$season, "-regular")
     
-    content <- content(response, "text", encoding = "UTF-8")
-    result <- fromJSON(content, flatten = T)
-    reactive$result <- result$playerinjuries$playerentry %>%
+    injury_result <- GET(paste0(base_url, "/player_injuries.json"), add_headers('Content-Type' = "application/json"),
+                    authenticate(msf_user, msf_pass)) %>%
+      content("text", encoding = "UTF-8") %>%
+      fromJSON(flatten = T)
+    reactive$injury <- injury_result$playerinjuries$playerentry %>%
       mutate(Name = paste(player.FirstName, player.LastName)) %>%
-      select(Name, player.ID, injury, team.Name, player.Position)
+      select(Name, player.ID, injury, player.Position, team.Name, team.ID)
+    
+    standings_result <- GET(paste0(base_url, "/overall_team_standings.json"), add_headers('Content-Type' = "application/json"),
+                            authenticate(msf_user, msf_pass)) %>%
+      content("text", encoding = "UTF-8") %>%
+      fromJSON(flatten = T)
+    reactive$standing <- standings_result$overallteamstandings$teamstandingsentry %>%
+      select(rank, team.ID, team.Name, team.Abbreviation)
+    
+    reactive$positions <- as.vector(unique(reactive$injury$player.Position))
+    
+    if (input$position_choice != "") {
+      reactive$injury_table <- filter(reactive$injury, player.Position == input$position_choice)
+      teams <- as.vector(unique(reactive$table$team_ID))
+    } else {
+      reactive$injury_table <- reactive$injury
+      reactive$team_table <- reactive$standing
+    }
   })
-
-  output$table <- renderDataTable(reactive$result)
+  
+  #=======================================================#
+  # Filters API Databases by when desired position changes#
+  #=======================================================#
+  
+  observeEvent(input$position_choice, {
+    if (input$position_choice != "") {
+      reactive$injury_table <- filter(reactive$injury, player.Position == input$position_choice)
+      teams <- as.vector(unique(reactive$table$team_ID))
+    } else {
+      reactive$injury_table <- reactive$injury
+      reactive$team_table <- reactive$standing
+    }
+  })
+  
+  output$positions <- renderText(reactive$positions)
+  output$injury_table <- renderTable(reactive$injury_table)
+  output$team_table <- renderTable(reactive$team_table)
 }
 
 shinyServer(func)
+
+
+
+
+
+
+
+
 
 
--- ui.R
+++ ui.R
@@ -5,10 +5,15 @@ ui <- fluidPage(
   titlePanel("NFL Injuries"),
   sidebarLayout(
     sidebarPanel(
-      radioButtons('season', "Choose season by year", c("2014-2015", "2015-2016", "2016-2017"))
+      radioButtons('season', "Choose season by year", c("2014-2015", "2015-2016", "2016-2017")),
+      textInput('position_choice', "Choose position")
     ),
     mainPanel(
-      dataTableOutput('table')
+      h2("Step 1: Choose Season Year"),
+      h2("Step 2: Choose one of the following positions"),
+      h4(textOutput('positions')),
+      tableOutput('injury_table'),
+      tableOutput('team_table')
     )
   )
 )
