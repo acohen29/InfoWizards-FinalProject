2585ccd872831b616a71d6692701aed416c7268a
Alexander Cohen <acohen29@uw.edu>
2018-05-30 17:20:55 -0700

    Cleaning text

---
+++ analysis.R
@@ -0,0 +1,51 @@
+landing <- "Welcome to the Info Wizards Football Analysis Program. 
+             This program allows you to explore data on football injuries, team success, 
+             salaries and the value of NFL draft picks. Data for this project was courtesy 
+             of https://www.mysportsfeeds.com. The goal of this project is to display the 
+             importance of data as it relates to injuries and the management of a professional 
+             football team. To display this, we chose to break the information down into several 
+             distinct categories for easier user accessibility. The first section presents 
+             data on NFL injuries and how those affect team performance. The overall goal of 
+             this section is to show the importance of particular positions for a football team 
+             as it relates to team performance. The second section presents data on NFL salaries 
+             based on positions. The first tab displayed how injuries affected team performance, 
+             the second tab builds upon this idea and examines how salaries relate to positional 
+             impacts on team performance. Finally, the third section of this report focuses on 
+             draft picks and how drafting players affects team performance. The report is crafted 
+             in such a way as to make these sections distinct. The different sections can be 
+             found under their corresponding tabs at the top of the page. You are presently at the 
+             Homepage."
+
+q1_analysis_1 <- "The question we set out to answer for this section was: 
+                                            How do injures to certain positions impact team success? 
+One of the most important players on a team is the running back. 
+A balanced offensive attack is often considered key to getting wins, 
+the most effective marker of team success. As evidenced by the 
+Seattle Seahawks of late, a lack of a rushing game can be a 
+serious issue in terms of helping a team to win. As a result, we 
+looked at running back injuries during the 2016-2017 season. During 
+that season, running backs were the position group that had the most 
+amount of injuries, something that was not unexpected given the 
+physicality of the position and the frequency with which running 
+backs are injured. What we found was that the average rank for teams 
+who sustained an injury to their starting running back was"
+
+q1_analysis_2 <- "compared to the league average average rank of 16. 
+                                          Interestingly, this data included a significant outlier. 
+One of the running backs, Jay Ajayi of the Dolphins, 
+went down with an injury, but despite this the team's rank 
+remained well above the average at 9, a full 7 points above league average. 
+This data point certainly seems to be an outlier, 
+especially since each of the other teams who had running 
+backs go down with an injury were ranked fairly low. The 
+importance of what we found was that teams who had injuries 
+at the RB position were `r mean - r mean` below the rest of 
+the league in terms of rank indicating that they were at a 
+disadvantage. While this experiment was performed with the 
+utmost caution to avoid bias, there are several factors that 
+should be mentioned as important. The sample size upon which the 
+t-test was taken was not very big only 4 players were listed according 
+to API that we used. Additionally, the NFL consists of only 32 which means 
+it is not exactly an ideally sized data set. However, the data that we 
+have found indicates an interesting trend in terms of the importance 
+of running backs."
--- ui.R
+++ ui.R
@@ -1,6 +1,7 @@
 library(shiny)
 library(httr)
 library(shinythemes)
+source("analysis.R")
 
 ui <- navbarPage(
   "NFL Injuries",
@@ -11,23 +12,7 @@ ui <- navbarPage(
 
   tabPanel("Landing",
            titlePanel("Info Wizards Info 201 AC3"),
-           p("Welcome to the Info Wizards Football Analysis Program. 
-             This program allows you to explore data on football injuries, team success, 
-             salaries and the value of NFL draft picks. Data for this project was courtesy 
-             of https://www.mysportsfeeds.com. The goal of this project is to display the 
-             importance of data as it relates to injuries and the management of a professional 
-             football team. To display this, we chose to break the information down into several 
-             distinct categories for easier user accessibility. The first section presents 
-             data on NFL injuries and how those affect team performance. The overall goal of 
-             this section is to show the importance of particular positions for a football team 
-             as it relates to team performance. The second section presents data on NFL salaries 
-             based on positions. The first tab displayed how injuries affected team performance, 
-             the second tab builds upon this idea and examines how salaries relate to positional 
-             impacts on team performance. Finally, the third section of this report focuses on 
-             draft picks and how drafting players affects team performance. The report is crafted 
-             in such a way as to make these sections distinct. The different sections can be 
-             found under their corresponding tabs at the top of the page. You are presently at the 
-             Homepage.")
+           p(landing)
            ),
 
   #============================================================================
@@ -47,9 +32,12 @@ ui <- navbarPage(
         h3("Step 1: Choose Season Year"),
         h3("Step 2: Choose one of the following positions"),
         h4(textOutput("positions")),
+        br(),
         plotOutput("plot")
       )
-    ), textOutput("text")
+    ),
+    br(),
+    textOutput("q1_analysis")
   ),
 
   #============================================================================
