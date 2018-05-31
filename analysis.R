7ef9b9db5dc83050c0b2aacdab60738b22b86cfc
Alexander Cohen <acohen29@uw.edu>
2018-05-31 11:48:54 -0700

    Add interactive, Clean data

--- analysis.R
+++ analysis.R
@@ -37,7 +37,7 @@ and the chosen position is RB. The question we set out to answer for this sectio
 q1_analysis_2 <- "compared to the league average average rank of 16.
 Interestingly, this data included a significant outlier.
 One of the running backs, Jay Ajayi of the Dolphins,
-went down with an injury, but despite this the team’s rank
+went down with an injury, but despite this the team's rank
 remained well above the average at 9, a full 7 points above league average.
 This data point certainly seems to be an outlier,
 especially since each of the other teams who had running
@@ -71,3 +71,51 @@ of the number of injuries. What we found that Wisconsin had "
 q2_analysis_5 <- "injuries while Texas had "
 
 q2_analysis_6 <- "injuries. Overall these results would appear to indicate that "
+
+
+q3_analysis_1 <- "Our Analysis revolved around two variables: 1) how much a player cost 2) how many 
+                   points said player earned. Fantasy football is a game in which people draft real 
+                   life players in hopes of outscoring their opponent. Points are earned by real life
+                   accolades and achievements such as touchowns, yards, etc. The bigger the game a 
+                   player has, the more points he earns for your team.
+                   
+                   Our focus revolved around DFS, a unique variation of fantasy football where instead
+                   of drafting in snake order at the beginning of the season, participants draft their
+                   lineup on a week to week basis. One can say that this method elminates the luck aspect
+                   of fantasy football. Later, we will depict how luck is still the biggest factor and
+                   it is here to stay."
+q3_analysis_2 <- "First, we took a look at our multiple datasets involving more than 10,000 entries
+                   per season. We selected and narrowed down the focus on two columns/categories. How
+                   much a player cost(DFS/Salary) and how many points said player earned the corresponding
+                   week. We then ran an r-squared test on these two variables and found that generally,
+                   you can expect an r-squared of .15-.2 which signals a very weak correlation, if 
+                   any exists at all. In other words, only 7% - 12% of the standard deiation can be
+                   explaiend by player price. the other 88% - 93% is caused by other factors, 
+                   mainly luck."
+q3_analysis_3 <- "Despite the fact that there was very little correlation between price and points 
+                   returned, we still think player price somewhat reflects their value.Football, both
+                   real and fantasy are determined not by one influential event but multiple small ones
+                   which add up. The difference between 10 points could be half a second too early or 
+                   half a step too late. Thus, there is bound to be some variance. Lastly, nobody in the 
+                   public has access to one of the most important aspects of this all: the coaching and
+                   play-calling. If NFL coaches and teams are trying to move erratically to prevent other
+                   worlds class professional athletes and coaches from predicting their plays, then its
+                   unlikely that the average spectator and fan stands a better shot. We only had 4 
+                   seasons worth of data to work with. As we accumulate more while technology improves
+                   simultaneously, more accurate models and predictions will be possible."
+
+conclusion_1 <- "As seen in the previous tabs, we found some interesting correlations in our project.
+While it would be incorrect to state with 100% certainty that all of the results that we have come to
+are guaranteed always to be true, we have found evidence that there may be a correlation between
+player injuries at certain positions and team performance. Unfortunately, we are not able to say with complete
+certianty that there is a correlation between geographical location and team rank or injuries. Interestingly,
+we found little correlation between player price and their actual point value. This was a surprising result,
+we had expected stars, who fantasy owners paid a lot for, to have a signficant impact in terms of performance.
+However, as stated, we feel that these result may be due to variance and that overall there may be a correlation
+between price and performance. So what do these results mean? What we have showed is that there are several
+factors that can potentially influence team performance, including injuries and geographical location, not to mention
+team managment, salary allocation and draft effictivness which we were unfortunately not able to explore given the 
+time constraint. In addition we found that price may have less of a correlation to performance that we thought. All of this
+indicates that football is a complex sport in which no one factor can be deemed to be conclusive. That said,
+we have found that proper valuation of players, and the avoidance of injuries may well be important in determining
+team success."
--- server.R
+++ server.R
@@ -3,6 +3,7 @@ library(httr)
 library(dplyr)
 library(jsonlite)
 library(ggplot2)
+library(maps)
 source("key.R")
 source("analysis.R")
 
@@ -14,21 +15,39 @@ func <- function(input, output) {
     left_join(data.frame(abbr = state.abb, state = state.name, stringsAsFactors = F), by = c("country.etc" = "abbr"))
   city_to_state$name <- substr(city_to_state$name, 0, nchar(city_to_state$name) - 3)
 
-  #============================================================================
-  # GETS the API Databases when the desired year changes
-  #============================================================================
-
   observeEvent(input$season, {
-    base_url <- paste0("https://api.mysportsfeeds.com/v1.2/pull/nfl/",
-                       input$season)
+    reactive$base_url <- paste0(
+      "https://api.mysportsfeeds.com/v1.2/pull/nfl/",
+      input$season
+    )
+  })
 
-    #==========================================================================
+  observeEvent(input$season_2, {
+    reactive$base_url <- paste0(
+      "https://api.mysportsfeeds.com/v1.2/pull/nfl/",
+      input$season_2
+    )
+  })
+
+  # ============================================================================
+  # GETS the API Databases when the desired year changes
+  # ============================================================================
+
+  observeEvent({
+    input$season
+    input$season_2
+  }, {
+
+
+    # ==========================================================================
     # API and manipulations to injury dataset
-    #==========================================================================
+    # ==========================================================================
 
-    injury_result <- GET(paste0(base_url, "/player_injuries.json"),
-                         add_headers('Content-Type' = "application/json"),
-                    authenticate(msf_user, msf_pass)) %>%
+    injury_result <- GET(
+      paste0(reactive$base_url, "/player_injuries.json"),
+      add_headers("Content-Type" = "application/json"),
+      authenticate(msf_user, msf_pass)
+    ) %>%
       content("text", encoding = "UTF-8") %>%
       fromJSON(flatten = T)
     reactive$injury <- injury_result$playerinjuries$playerentry %>%
@@ -38,110 +57,140 @@ func <- function(input, output) {
 
     reactive$positions <- as.vector(unique(reactive$injury$player.Position))
 
-    #==========================================================================
+    # ==========================================================================
     # API and manipulations to team standings dataset
-    #==========================================================================
+    # ==========================================================================
 
-    standings_result <- GET(paste0(base_url, "/overall_team_standings.json"),
-                            add_headers('Content-Type' = "application/json"),
-                            authenticate(msf_user, msf_pass)) %>%
+    standings_result <- GET(
+      paste0(reactive$base_url, "/overall_team_standings.json"),
+      add_headers("Content-Type" = "application/json"),
+      authenticate(msf_user, msf_pass)
+    ) %>%
       content("text", encoding = "UTF-8") %>%
       fromJSON(flatten = T)
     reactive$standing <- standings_result$overallteamstandings$teamstandingsentry %>%
       select(rank, team.ID, team.Name, team.City, team.Abbreviation)
+    reactive$standing$rank <- as.numeric(reactive$standing$rank)
 
     reactive$rank_injury <- left_join(reactive$injury, reactive$standing, by = "team.ID")
   })
 
-  #============================================================================
+  # ============================================================================
   # Filters datasets when desired position or season changes
-  #============================================================================
+  # ============================================================================
 
   observeEvent({
     input$season
-    input$position_choice}, {
-      if (input$position_choice != "") {
-        data_plot_1 <- filter(reactive$rank_injury, player.Position == input$position_choice)
-        position <- input$position_choice
-      } else {
-        data_plot_1 <- reactive$rank_injury
-        position <- "All Positions"
-      }
-      reactive$injured_mean <- data_plot_1$rank %>%
-        unique() %>%
-        as.numeric() %>%
-        mean()
+    input$position_choice
+  }, {
+    if (input$position_choice != "") {
+      reactive$data_plot_1 <- filter(reactive$rank_injury, player.Position == input$position_choice)
+      position <- input$position_choice
+    } else {
+      reactive$data_plot_1 <- reactive$rank_injury
+      position <- "All Positions"
+    }
 
-      #========================================================================
-      # Code for Question 1 Plot
-      #========================================================================
+    temp_table <- group_by(reactive$data_plot_1, rank) %>%
+      summarize(num = n())
 
-      reactive$plot <- ggplot(data_plot_1) +
-        geom_bar(mapping = aes(x = team.Name.x, y = as.numeric(rank), fill = team.Abbreviation),
-                 width = 1, stat = "identity") +
-        scale_fill_brewer(palette = "Blues") +
-        guides(fill = "none") +
-        labs(
-          title = paste0("Team ranks (for those with injuries at ", position, ")
+    reactive$data_plot_1 <- left_join(reactive$data_plot_1, temp_table, by = "rank")
+
+    reactive$injured_mean <- reactive$data_plot_1$rank %>%
+      unique() %>%
+      as.numeric() %>%
+      mean()
+
+    # ========================================================================
+    # Code for Question 1 Plot
+    # ========================================================================
+
+    reactive$plot <- ggplot(reactive$data_plot_1) +
+      geom_point(mapping = aes(x = team.Name.x, y = rank)) +
+      geom_col(
+        mapping = aes(x = team.Name.x, y = (rank / num), fill = team.Abbreviation),
+        width = 1
+      ) +
+      scale_fill_brewer(palette = "Blues") +
+      guides(fill = "none") +
+      labs(
+        title = paste0("Team ranks (for those with injuries at ", position, ")
                          compared to the league average"),
-          x = "Team Names",
-          y = "Rank")
-    })
+        x = "Team Names",
+        y = "Rank"
+      )
+  })
 
-  #============================================================================
+  # ============================================================================
   # Choice for Map Data Set
-  #============================================================================
+  # ============================================================================
   observeEvent({
-    input$season
-    input$map_selection}, {
+    input$season_2
+    input$map_selection
+  }, {
+    us_map <- map_data("state")
+    us_map$region <- stringr::str_to_title(us_map$region)
 
-      us_map <- map_data("state")
-      us_map$region <- stringr::str_to_title(us_map$region)
+    if (input$map_selection == 1) {
+      map <- left_join(reactive$standing, city_to_state, by = c("team.City" = "name"))
+      map <- left_join(us_map, map, by = c("region" = "state"))
+      reactive$map <- ggplot(map) +
+        geom_polygon(mapping = aes(
+          x = long, y = lat, group = region,
+          fill = rank, color = "black"
+        )) +
+        coord_quickmap() +
+        guides(fill = F, color = F) +
+        scale_color_brewer(palette = "Greys") +
+        scale_x_continuous(breaks = NULL, name = NULL) +
+        scale_y_continuous(breaks = NULL, name = NULL) +
+        labs(
+          title = "Team Rankings by State"
+        )
+    } else {
+      map <- left_join(reactive$injury, city_to_state, by = c("team.City" = "name"))
+      map <- left_join(us_map, map, by = c("region" = "state"))
+      reactive$map <- ggplot(map) +
+        geom_polygon(mapping = aes(
+          x = long, y = lat, group = region,
+          fill = has_injury, color = "black"
+        )) +
+        coord_quickmap() +
+        guides(fill = F, color = F) +
+        scale_color_brewer(palette = "Greys") +
+        scale_x_continuous(breaks = NULL, name = NULL) +
+        scale_y_continuous(breaks = NULL, name = NULL) +
+        labs(
+          title = "Player Injuries by State"
+        )
+    }
+  })
 
-      if (input$map_selection == 1) {
-        map <- left_join(reactive$standing, city_to_state, by = c("team.City" = "name"))
-        map <- left_join(us_map, map, by = c("region" = "state"))
-        reactive$map <- ggplot(map) +
-            geom_polygon(mapping = aes(
-              x = long, y = lat, group = region,
-              fill = rank, color = "black"
-            )) +
-            coord_quickmap() +
-            guides(fill = F, color = F) +
-            scale_x_continuous(breaks = NULL, name = NULL) +
-            scale_y_continuous(breaks = NULL, name = NULL) +
-            labs(
-              title = "Team Rankings by State"
-            )
-      } else {
-        map <- left_join(reactive$injury, city_to_state, by = c("team.City" = "name"))
-        map <- left_join(us_map, map, by = c("region" = "state"))
-        reactive$map <- ggplot(map) +
-          geom_polygon(mapping = aes(
-            x = long, y = lat, group = region,
-            fill = has_injury, color = "black"
-          )) +
-          coord_quickmap() +
-          guides(fill = F, color = F) +
-          scale_x_continuous(breaks = NULL, name = NULL) +
-          scale_y_continuous(breaks = NULL, name = NULL) +
-          labs(
-            title = "Player Injuries by State"
-          )
-      }
-    })
+  # ============================================================================
+  # On plot Click
+  # ============================================================================
 
-  #============================================================================
+  observeEvent(input$click, {
+    selected <- data.frame(nearPoints(reactive$data_plot_1, input$click))
+    selected <- select(selected, Name, injury, team.Name.x)
+    colnames(selected) <- c("Name", "Injury", "Team")
+    reactive$player <- selected
+  })
+
+
+  # ============================================================================
   # Outputs
-  #============================================================================
+  # ============================================================================
 
   output$positions <- renderText(reactive$positions)
   output$plot <- renderPlot(reactive$plot)
   output$q1_analysis <- renderText(paste(q1_analysis_1, reactive$injured_mean, q1_analysis_2))
   output$map <- renderPlot(reactive$map)
-  output$q2_analysis <- renderText(paste(q2_analysis_1, q2_analysis_2, q2_analysis_3, q2_analysis_4, 
-                                         q2_analysis_5, q2_analysis_6))
-
+  output$q2_analysis <- renderText(paste(
+    q2_analysis_1, q2_analysis_2, q2_analysis_3, q2_analysis_4,
+    q2_analysis_5, q2_analysis_6
+  ))
+  output$player <- renderTable(reactive$player)
 }
 
 shinyServer(func)
